12f2301d4ae12e2fe7373dd7f0038e811b32079d
MatthewJCifue99 <7mcifuentes@stpatrick.org>
2016-10-07 10:28:30 -0500

    fnished

---
+++ octocats.js
@@ -0,0 +1,14 @@
+function octocat(n,b){
+this.name = n
+this.arms = b
+
+ this.slap = slap.function(){
+   for(var b=0;b<this.arms;b++){
+   console.log("SLAP")
+ }
+ };
+ }
+
+ new mattcat = new octocat("matthew", 6)
+ new cifuecat = new octocat("cifuentes",10)
+ new mattycat = new octocat("matty", 20)
